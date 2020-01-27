a408e11ec78c8f6bda1420eed1e1224c660c19d1
CrossroadsMan <crossroadsman@gmail.com>
2020-01-27 15:20:54 -0700

    Add basic template settings

--- django_cms/official/myproject/settings.py
+++ django_cms/official/myproject/settings.py
@@ -97,7 +97,9 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',  # required by Django CMS
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -173,3 +175,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+# see Django docs and Django CMS docs:
+# http://docs.django-cms.org/en/latest/how_to/install.html#media-and-static-file-handling
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Other DjangoCMS-Specific settings:
+CMS_TEMPLATES = [
+    ('home.html', 'Home page template'),
+]
